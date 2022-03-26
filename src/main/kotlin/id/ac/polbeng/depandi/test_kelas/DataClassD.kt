f738d9a926f2f27f5e05fc6d1ebb01881d19d4b1
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-26 11:24:47 +0700

    Percobaan Data class toString() method

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/DataClassD.kt
@@ -0,0 +1,7 @@
+package id.ac.polbeng.depandi.test_kelas
+//deklrasi dan inisialisasi Data class toString() method
+// fungsi toString pada data kelas mengembalikan nilai kembalian string yan meruapakan representasi dari objek
+fun main() {
+    val hazimah = Student("Hazimah", 17)
+    println(hazimah.toString())
+}
