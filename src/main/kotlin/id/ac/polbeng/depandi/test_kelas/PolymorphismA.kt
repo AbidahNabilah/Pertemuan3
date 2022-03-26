1e02a4979d95a45d56fc1a65673a0f56ad21a208
Abidah Nabilah <abdhnabilah@gmail.com>
2022-03-26 11:35:46 +0700

    Percobaan Static Polymorphism (Overloading Function)

---
+++ src/main/kotlin/id/ac/polbeng/depandi/test_kelas/PolymorphismA.kt
@@ -0,0 +1,34 @@
+package id.ac.polbeng.depandi.test_kelas
+// deklarasi dan inisisalisasi Polymorphisme
+// menggunakan static polymorphsim  terjadi ketika mendefinisiskan beberpa fungsi overloading dengan nama
+// yang sama tetapi berbeda karateristik parameter (jenis type dan jumlah parameter)
+fun printNumber(n : Number){
+    println("Using printNumber(n : Number)")
+    println(n.toString() + "\n")
+}
+
+fun printNumber(n : Int){
+    println("Using printNumber(n : Int)")
+    println(n.toString() + "\n")
+}
+
+fun printNumber(n : Double)
+{
+    println("Using printNumber(n : Double)")
+    println(n.toString()+"\n")
+}
+
+fun printNumber(n:Int, str:String){
+    println("Using printNumber(n:Int, str:String)")
+    println(str + n.toString() + "\n")
+}
+fun main() {
+    val a: Number = 99
+    val b= 1
+    val c= 3.1
+    printNumber(a)
+    printNumber(b)
+    printNumber(c)
+    printNumber(b, "Overloading function ")
+
+}
